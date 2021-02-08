6dfbc14ebf32610a4b170b767b195ac4401ae3fb
Janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2021-02-08 23:11:14 +0100

    prototype works

--- Main.py
+++ Main.py
@@ -1,31 +1,89 @@
+import os
 import numpy as np
-import python_speech_features as speech
-import signal_parametrization
-import time
-import sklearn.mixture._gaussian_mixture
+import soundfile as sf
 
-print("start")
-wektoro = np.random.rand(50000)
-t = time.time()
-framed = signal_parametrization.framing(
-    wektoro, 16000, 0.025, 0.01, np.hamming)
-t1 = time.time() - t
-t = time.time()
-framed2 = speech.sigproc.framesig(wektoro, 400, 160, np.hamming)
-t2 = time.time() - t
+from sklearn.mixture import GaussianMixture
+from python_speech_features import mfcc
 
-mfcc = signal_parametrization.mfcc(wektoro, rate=41000, winlen=0.01,
-                                   winstep=0.005,
-                                   winfunc=lambda x: np.ones((x,)),
-                                   ndft=512, lowfreq=0,
-                                   highfreq=20500, nmfilter=41,
-                                   ncep=13, Energy=True)
-mfcc2 = speech.mfcc(wektoro, samplerate=41000, winlen=0.01, winstep=0.005,
-                    winfunc=lambda x: np.ones((x,)), nfft=512, lowfreq=0,
-                    highfreq=20500, nfilt=41, numcep=13, appendEnergy=True,
-                    preemph=0, ceplifter=0)
+n_components = 12
+covariance_type = 'diag'
+tol = 1e-3
+max_iter = 100
 
-gmm = sklearn.mixture._gaussian_mixture.GaussianMixture(n_components=100,
-                                                        init_params='random')
-gmm.fit(mfcc)
-print("end")
+validation_length = 100
+
+
+def flattenToMono(data):
+    output = np.copy(data)
+    if len(data.shape) == 2:
+        output = output[:, 0].flatten()
+    else:
+        output = output.flatten()
+    return output
+
+
+def loadFiles(path):
+    outputData, outputSampleRates = list(), list()
+    for (directoryPath, directoryName, fileName) in os.walk(path):
+        for file in fileName:
+            if file.endswith('.wav'):
+                tempPath = os.getcwd() + '/' + os.path.join(directoryPath,
+                                                            file)
+                data, samplerate = sf.read(tempPath)
+                outputData.append(flattenToMono(data))
+                outputSampleRates.append(samplerate)
+    return outputData, outputSampleRates
+
+
+def main():
+    path =  './data'
+    print("Path: %s" % path)
+    recordings, samplerates = loadFiles(path)
+    print("recording length %d" % len(recordings))
+
+    # Classification
+    classificators = list()
+    for recordingIndex in range(len(recordings)):
+        classificators.append(
+            GaussianMixture(n_components=n_components,
+                            covariance_type=covariance_type,
+                            tol=tol, max_iter=max_iter))
+
+    features = list()
+    # Features
+    for recordingIndex in range(len(recordings)):
+        features.append(mfcc(recordings[recordingIndex],
+                             samplerate=samplerates[recordingIndex],
+                             winfunc=lambda x: np.hamming(x)))
+
+    # Trainning
+    for featuresIndex in range(len(features)):
+        classificators[featuresIndex].fit(
+            features[featuresIndex])
+
+    # Validation Test
+    score = 0
+    for testNumber in range(validation_length):
+        currentRecordingNumber = testNumber % 2
+        print("current index: %d" % currentRecordingNumber)
+        recording = recordings[currentRecordingNumber]
+        currentMfcc = mfcc(recording,
+                           samplerate=samplerates[currentRecordingNumber],
+                           winfunc=lambda x: np.hamming(x))
+
+        loglikelihoods = list()
+        for classificatorIndex in range(len(classificators)):
+            loglikelihoods.append(
+                classificators[classificatorIndex].score(currentMfcc))
+
+        max_index = loglikelihoods.index(max(loglikelihoods))
+        if max_index == currentRecordingNumber:
+            score += 1
+
+    # print score
+    print("The overall score: " +
+          str(float(score) / validation_length * 100))
+
+
+if __name__ == "__main__":
+    main()
